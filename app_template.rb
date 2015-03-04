ba59dc74f9663e71df3d1cd5cd9fa32e712d7431
taga <taga@321sa.net>
2015-03-04 22:35:19 +0900

    ひとまずgemとかいろいろ入れてみた

--- app_template.rb
+++ app_template.rb
@@ -1,24 +1,57 @@
+@ruby_version = ask("ruby version?(Ex. 2.1.5)")
+# @database_user = ask("database user?")
+
+# clean file
+# ----------------------------------------------------------------
+run 'rm README.rdoc'
+run 'touch README.md'
+
+append_file '.gitignore', <<-END
+#rubymine
+.idea/*
+END
+
 # gem
 # ----------------------------------------------------------------
+remove_file 'Gemfile'
+create_file 'Gemfile'
+add_source 'https://rubygems.org'
+append_file 'Gemfile', <<-END
+ruby '#{@ruby_version}'#herokuのため
+END
+gem 'rails'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jquery-turbolinks'
 gem 'haml-rails'
 gem 'bootstrap-sass'
 gem 'font-awesome-rails'
 gem 'simple_form'
 gem 'carrierwave'
-gem 'rmagick'
+gem 'mini_magick'
+gem 'fog'
 gem 'gretel'
 gem 'kaminari'
 gem 'devise'
 gem 'devise-i18n'
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
+gem 'omniauth-github'
 gem 'cancan'
-gem 'thin'
-gem 'record_with_operator'
-
-
+gem 'squeel'
+# gem 'record_with_operator'
+# gem 'enum_help'
+gem 'pg'
+gem 'dotenv-rails'
+gem 'sprockets'
 
 gem_group :development do
   gem 'better_errors'
-  gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
+  gem 'binding_of_caller'
   gem 'guard-bundler'
   gem 'guard-rails'
   gem 'guard-rspec'
@@ -28,11 +61,14 @@ gem_group :development do
   gem 'spring'
   gem 'spring-commands-rspec'
   gem 'annotate'
+  gem 'hirb'
+  gem 'hirb-unicode'
+  gem 'pry-rails'
+  gem 'html2haml'
 end
 
-
 gem_group :development, :test do
-  gem 'sqlite3'
+  gem 'thin'
   gem 'rspec-rails'
   gem 'factory_girl_rails'
 end
@@ -45,14 +81,40 @@ gem_group :test do
   gem 'selenium-webdriver'    
 end
 
-gem_group :staging do
-  gem 'rails_12factor'
-end
-
 gem_group :staging, :production do
-#  gem 'pg'
+  gem 'unicorn'
+  gem 'rails_12factor'#herokuでの実行のため
 end
 
+# run "bundle install" #自動で実施されるぽい
+
+#database.yml
+# gsub_file 'config/database.yml', /#username: /, ''
+
+# Generators
+# ----------------------------------------------------------------
+generate 'simple_form:install --bootstrap'
+
+#devise
+generate 'devise:install'
+run 'rails generate devise user'
+
+# set config/application.rb
+application  do
+  %q{
+    # Set timezone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    # 日本語化
+    I18n.enforce_available_locales = true
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+  }
+end
+
+# set Japanese locale
+# brew install wget が必要(wgetが無ければ)
+run 'wget https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml -P config/locales/'
 
 # git
 # ----------------------------------------------------------------
